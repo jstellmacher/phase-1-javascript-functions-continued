5ceeb05f4690fedaafc75780ef9b36e84b8f5ced
Jai <jaijai.stell@gmail.com>
2023-03-21 08:20:36 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// jai stellmachers code!
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function wrapAdjective(style = "*") {
+  return function wrapSpecail(adjective = "special") {
+    return `You are ${style}${adjective}${style}!`;
+  };
+}
